b20a696bb67ffee7265d902d27478699b56604bb
Alfredo Hernández <aldomann.designs@gmail.com>
2017-04-27 20:05:40 +0200

    Proper structure to easily test different data sets

--- qif_solve_cpp_from_file.R
+++ qif_solve_cpp_from_file.R
@@ -1,28 +1,44 @@
 library(tidyverse)
 library(data.table)
-# library(Rcpp)
 
 # Calculations ---------------------------------------------
 
-# sourceCpp("cpp/qif_solve_simple_ask.cpp")
+get_qif.data <- function(){ # Manually select data file
+	v.qif <- t(fread("cpp/v_avg_dyn.dat", header = F))
+	# v.qif <- t(fread("cpp/v_avg10.dat", header = F))
+	v.qif <- v.qif[-nrow(v.qif),] # Remove last row
+	v.qif <- data.frame(v.avg = v.qif)
+	return(v.qif)
+}
 
-v.qif <- t(fread("cpp/v_avg.dat", header = F))
-v.qif <- v.qif[-nrow(v.qif),] # Remove last row
-v.qif <- data.frame(v.avg = v.qif)
+v.qif <- get_qif.data()
 
-source("fre_vs_qif_test.R")
+get_fre.data <- function(){
+	source("fre_vs_qif_test.R")
+}
 
 # Data visualisation ---------------------------------------
 
-# ggplot(v.qif)+
-# 	geom_line(mapping = aes(y = v.avg, x = seq(1, length(v.avg))),
-# 						colour = "darkorange") +
-# 	labs(x = "Time (s)", y = "v")
+get_vplot <- function(plot.type = "qif"){
+	if (plot.type == "qif") { # Just QIF plot
+		ggplot(v.qif)+
+			geom_line(mapping = aes(y = v.avg, x = seq(1, length(v.avg))),
+								colour = "darkorange") +
+			labs(x = "Time (s)", y = "v")
+	}
+	else if (plot.type == "fre+qif") { # Comparison with FRE model
+		get_fre.data()
+		out.fre.qif <- out.fre
+		out.fre.qif$v.agv <- v.qif$v.avg
 
-out.fre.qif <- out.fre
-out.fre.qif$v.agv <- v.qif$v.avg
+		ggplot(out.fre.qif)+
+			geom_line(mapping = aes(x = time, y = v.agv), colour = "black") +
+			geom_line(mapping = aes(x = time, y = v), colour = "darkorange") +
+			labs(x = "Time (s)", y = "v")
+	}
+}
 
-ggplot(out.fre.qif)+
-	geom_line(mapping = aes(x = time, y = v.agv), colour = "black") +
-	geom_line(mapping = aes(x = time, y = v), colour = "darkorange") +
-	labs(x = "Time (s)", y = "v")
+# get_vplot(plot.type = "fre+qif")
+get_vplot()
+
+
