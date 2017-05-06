0966eb25ea9633f9275a914f7648855d25c6bf0c
Alfredo Hernández <aldomann.designs@gmail.com>
2017-05-06 05:36:43 +0200

    Clean up code

--- cpp/qif_solve.cpp
+++ cpp/qif_solve.cpp
@@ -3,10 +3,7 @@
 using namespace std;
 
 #include <vector>
-
 #include <cmath>
-using std::pow;
-using std::tan;
 #include <cstdlib> // rand()
 #include <ctime>   // time()
 
@@ -20,13 +17,13 @@ int main () {
 	double t_final = 10.0, t_init = 0.0;
 	double h = 0.0001;
 	int steps = int((t_final - t_init)/h);
-	// double refract_steps = int( 1/(vp * h) );
+	// double refract_steps = int(1/(vp * h));
 	double refract_steps = 5; // For testing purposes
 
 	// Initialize eta vector
 	vector<double> eta(neurons);
-	srand((unsigned)time(0));
-	for ( int n = 0; n < neurons; n++ ) {
+	time_t srand((unsigned)time(0));
+	for (int n = 0; n < neurons; n++) {
 		eta[n] = tan(M_PI*(((double) rand() / (RAND_MAX))- 0.5 )) - 5;
 	}
 
@@ -38,22 +35,17 @@ int main () {
 
 	// Initialize mean membrane potential vector
 	vector<double> v_avg(steps+1);
-	for (int i = 0; i < steps+1; i++) {
-		v_avg[i] = v0;
-	}
+	v_avg[0] = v0;
 
 	// Initialize membrane potential matrix
 	vector< vector<double> > v;
-	v.resize( 2 , vector<double>( neurons ) );
+	v.resize(2 , vector<double>(neurons));
 	for (int n = 0; n < neurons; n++) { // Initial membrane potential satate
 		v[0][n] = v0;
 	}
 
 	// Initialize spike_times vector
-	vector<double> spike_times(neurons);
-		for (int n = 0; n < neurons; n++) {
-		spike_times[n] = 0;
-	}
+	vector<double> spike_times(neurons, 0);
 
 	// Open file and write first line
 	ofstream myfile ("v_avg.dat"); // Open file
@@ -62,11 +54,11 @@ int main () {
 	// Loop
 	for (int i = 1; i < steps + 1; i++) {
 		for (int n = 0; n < neurons; n++) {
-			if(v[0][n] >= vp ) {
+			if(v[0][n] >= vp) { // Emit a spike
 				v[1][n] = -vp;
-				spike_times[n] = i; // Needed to calculate refractory times
+				spike_times[n] = i;
 			}
-			else if ( spike_times[n] == 0 ){ // Normal evolution
+			else if (spike_times[n] == 0){ // Normal evolution
 				v[1][n] = v[0][n] + h * ( pow(v[0][n], 2) + I[i] + eta[n] );
 			}
 			else if (spike_times[n] + refract_steps < i) { // Reset refractory time
@@ -81,5 +73,3 @@ int main () {
 
 	return 0;
 }
-
-
