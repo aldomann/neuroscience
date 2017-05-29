a6026d27a74e39295a79afe1960ace7d12e7c875
Alfredo Hernández <aldomann.designs@gmail.com>
2017-05-29 16:53:02 +0200

    Use ggmatrix (from GGally) instead of multiplots

--- qif_solve_cpp_from_file.R
+++ qif_solve_cpp_from_file.R
@@ -3,18 +3,20 @@
 
 library(tidyverse)
 library(data.table)
+library(GGally)
 
 # Calculations ---------------------------------------------
 
 sys.time.init <- Sys.time()
 
 # Compile C++ file if needed
-compile = T
+compile = F
 if (compile == T) {
 	system("cd cpp; g++ -o run_solve qif_solve.cpp; ./run_solve")
 }
 
-read_qif_data <- function(){ # Manually select data file
+# Read QIF data files and create data frames
+read_qif_data <- function(){
 	# Read raw data
 	v.data <- t(fread("cpp/v_avg.dat", header = F))
 	r.data <- t(fread("cpp/r_avg.dat", header = F))
@@ -27,12 +29,31 @@ read_qif_data <- function(){ # Manually select data file
 	return(qif.data)
 }
 
-qif.data <- read_qif_data()
+read_raster_data <- function(){
+	# Read raw data
+	raster.data <- fread("cpp/raster.dat", header = F)
+	# Clean data frame
+	raster.data <- raster.data %>%
+		rename(time = V1 , neuron = V2)
+	return(raster.data)
+}
 
+qif.data <- read_qif_data()
+raster.data <- read_raster_data()
+
+sys.time.comp.final <- Sys.time()
+
+# Simulate neurons via FREs
 get_fre_data <- function(){
 	source("fre_vs_qif_test.R")
 }
-sys.time.comp.final <- Sys.time()
+
+get_fre_data()
+out.fre.qif <- out.fre
+out.fre.qif$v.agv <- qif.data$v.avg
+out.fre.qif$r.agv <- qif.data$r.avg
+
+sys.time.fre.final <- Sys.time()
 
 # Data visualisation ---------------------------------------
 
@@ -43,30 +64,80 @@ get_plot_qif <- function(){
 		labs(x = "Time (s)", y = "v")
 }
 
-get_plots <- function(){
-	get_fre_data()
-	out.fre.qif <- out.fre
-	out.fre.qif$v.agv <- qif.data$v.avg
-	out.fre.qif$r.agv <- qif.data$r.avg
+# get_plots_legacy <- function(){
+# 	get_fre_data()
+# 	out.fre.qif <- out.fre
+# 	out.fre.qif$v.agv <- qif.data$v.avg
+# 	out.fre.qif$r.agv <- qif.data$r.avg
+#
+# 	plot_vt <- ggplot(out.fre.qif)+
+# 		geom_line(aes(x = time, y = v.agv), colour = "black") +
+# 		geom_line(aes(x = time, y = v), colour = "darkorange") +
+# 		labs(y = "v") +
+# 		theme(axis.title.x=element_blank(),
+# 					axis.text.x=element_blank(),
+# 					axis.ticks.x=element_blank())
+#
+# 	plot_rt <- ggplot(out.fre.qif)+
+# 		geom_line(aes(x = time, y = r.agv), colour = "black") +
+# 		geom_line(aes(x = time, y = r), colour = "darkorange") +
+# 		labs(y = "r") +
+# 		theme(axis.title.x=element_blank(),
+# 					axis.text.x=element_blank(),
+# 					axis.ticks.x=element_blank())
+#
+# 	plot_raster <- ggplot(raster.data) +
+# 		geom_point(aes(x = time, y = neuron), colour = "black", size = 0.2) +
+# 		expand_limits(x = 0) +
+# 		labs(y = "Neuron index") +
+# 		theme(axis.title.x=element_blank(),
+# 					axis.text.x=element_blank(),
+# 					axis.ticks.x=element_blank())
+#
+# 	plot_curr <- ggplot(current)+
+# 		geom_line(mapping = aes(x = times, y = I)) +
+# 		labs(x = "Time (s)", y = "I(t)")
+#
+# 	layout <- matrix(c(1,1,1,2,2,2,3,3,3,4,4), nrow = 11, byrow = TRUE)
+# 	multiplot(plotlist = list(plot_rt, plot_vt, plot_raster, plot_curr), layout = layout)
+# }
 
+get_plots <- function(){
 	plot_vt <- ggplot(out.fre.qif)+
-		geom_line(mapping = aes(x = time, y = v.agv), colour = "black") +
-		geom_line(mapping = aes(x = time, y = v), colour = "darkorange") +
+		geom_line(aes(x = time, y = v.agv), colour = "black") +
+		geom_line(aes(x = time, y = v), colour = "darkorange") +
 		labs(x = "Time (s)", y = "v")
 
 	plot_rt <- ggplot(out.fre.qif)+
-		geom_line(mapping = aes(x = time, y = r.agv), colour = "black") +
-		geom_line(mapping = aes(x = time, y = r), colour = "darkorange") +
+		geom_line(aes(x = time, y = r.agv), colour = "black") +
+		geom_line(aes(x = time, y = r), colour = "darkorange") +
 		labs(x = "Time (s)", y = "r")
 
-	layout <- matrix(c(1,2), nrow = 2, byrow = TRUE)
-	multiplot(plotlist = list(plot_rt, plot_vt), layout = layout)
+	plot_curr <- ggplot(current)+
+		geom_line(mapping = aes(x = times, y = I)) +
+		labs(x = "Time (s)", y = "I(t)")
+
+	plot_raster <- ggplot(raster.data) +
+		geom_point(aes(x = time, y = neuron), colour = "black", size = 0.2) +
+		expand_limits(x = 0) +
+		labs(x = "Time (s)", y = "Neuron index")
+
+	pm <- ggmatrix(list(plot_rt, plot_vt, plot_raster, plot_curr),
+								 nrow = 4, ncol = 1,
+								 yAxisLabels = c("r", "v", "Neuron index", "I(t)"),
+								 xlab = "Time (s)") +
+		theme( #strip.background = element_rect(fill = "white"),
+			strip.placement = "outside")
+	pm
 }
 
 # get_plot_qif()
+# get_plots_legacy()
 get_plots()
 
 sys.time.final <- Sys.time()
 
+# Print compilation times
 sys.time.comp.final - sys.time.init
+sys.time.fre.final - sys.time.comp.final
 sys.time.final - sys.time.init
